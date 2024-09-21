bf42d86bce424f5f2bbdb0193bedf48705f8a5fd
kummari-pavan <kpavan892002@gmail.com>
2024-09-22 00:50:06 +0530

    [PAVAN]ADD: adding UC-1_Emp_Presence_Check

--- Home/ewc.js
+++ Home/ewc.js
@@ -1,1 +1,14 @@
 console.log("Master Branch")
+// UC 1: Check if Employee is Present or Absent
+function checkEmployeeAttendance() {
+    const IS_PRESENT = 1;
+    let attendance = Math.floor(Math.random() * 2);
+    if (attendance === IS_PRESENT) {
+        console.log("Employee is Present");
+    } else {
+        console.log("Employee is Absent");
+    }
+}
+
+// Call the function
+checkEmployeeAttendance();
